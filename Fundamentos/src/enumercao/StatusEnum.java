17071620038d7827b09d66c2e03e715d7cd7deaa
Daniel Santos <daniel.gsantos@aluno.senai.br>
2023-08-17 10:21:50 -0300

    aprendendo enumeracao em Java

---
+++ Fundamentos/src/enumercao/StatusEnum.java
@@ -0,0 +1,20 @@
+
+ 
+package enumercao;
+
+/**
+ *
+ * @author Aluno
+ */
+public enum StatusEnum {
+    EM_PREPARACAO("Em preparação"),
+    EM_TRANSPORTE("Em transporte"),
+    ENTREGUE("Entregue ao cliente");
+    private String status;
+    private StatusEnum(String status){
+        this.status = status;
+    }
+    public String getStatus(){
+        return this.status;
+    }
+}
